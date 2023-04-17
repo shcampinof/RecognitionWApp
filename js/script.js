33bf6cbfe14a50cf48b5703596aaa1c3db4cfce1
Sebastian Campiño <seb4sttian@gmail.com>
2023-04-17 16:14:58 -0500

    frontend updated

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 const video = document.getElementById("video");
 
 Promise.all([
-  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
-  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.ssdMobilenetv1.loadFromUri("./models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
 ])
   .then(startWebcam)
   .then(faceRecognition);
